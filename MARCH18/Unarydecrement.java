220387bf198c6a80e01bfc50555e7dc221dd5a18
punithreddy84 <ranabothupunithreddy@gmail.com>
2025-03-18 11:21:48 +0530

    Add files via upload

---
+++ MARCH18/Unarydecrement.java
@@ -0,0 +1,14 @@
+class Unarydecrement{
+	public static void main(String[]args){
+		int a = 10;
+		System.out.println(--a + a);
+		a=10;
+		System.out.println(a + --a);
+		a=10;
+		System.out.println(a-- + a);
+		a=10;
+		System.out.println(a + a--);
+		a=10;
+		System.out.println(--a + --a);
+	}
+}
