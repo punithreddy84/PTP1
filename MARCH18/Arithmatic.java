5a931f565f332359a867c19846ca4ef7bd35580f
punithreddy84 <ranabothupunithreddy@gmail.com>
2025-03-18 11:42:32 +0530

    Add files via upload

---
+++ MARCH18/Arithmatic.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Arithmatic{
+	public static void main(String[]args){
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		System.out.println("addtion"+(a+b));
+		System.out.println("sub"+(a-b));
+		System.out.println("muit"+(a*b));
+		System.out.println("divison"+(a/b));
+		System.out.println("modulo"+(a%b));
+
+	}
+}
